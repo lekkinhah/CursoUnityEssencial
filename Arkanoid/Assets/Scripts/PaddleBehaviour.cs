db3d1f1e682acec3f4b3a54c1e69aec9680be6dc
lekkinhah <lekkinhah@gmail.com>
2018-03-15 13:18:35 -0300

    Limits and command with arrow

---
+++ Arkanoid/Assets/Scripts/PaddleBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehaviour : MonoBehaviour {
+
+	public float screenLimit;
+	public float speed = 0.2f;
+	//int direction = 1;
+	
+    float widthPaddle;
+
+	// Use this for initialization
+	void Start () {
+		widthPaddle = GetComponent<SpriteRenderer> ().size.x; 
+	}
+	
+	void Update () {
+        Vector2 currentPosition = transform.position;
+
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            currentPosition.x -= speed;
+            Debug.Log ("left");
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+            currentPosition.x += speed;	
+            Debug.Log ("right");
+		}
+
+		if (currentPosition.x < screenLimit-widthPaddle/2 && currentPosition.x > -(screenLimit - widthPaddle / 2)){
+            Debug.Log("entrou no if de movimento");
+            transform.position = currentPosition;
+        }
+
+	}
+}
