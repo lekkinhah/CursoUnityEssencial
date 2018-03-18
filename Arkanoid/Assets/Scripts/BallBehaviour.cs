2310ae607ab3e8b44726e945cd11c48448fdfb9f
lekkinhah <lekkinhah@gmail.com>
2018-03-18 11:17:20 -0300

    Hit changes with sprites

--- Arkanoid/Assets/Scripts/BallBehaviour.cs
+++ Arkanoid/Assets/Scripts/BallBehaviour.cs
@@ -6,12 +6,30 @@ public class BallBehaviour : MonoBehaviour {
 
 	public float startSpeed;
 
+
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody2D>().velocity = (Vector2.up+Vector2.right)*startSpeed;
-	}
-	
-	void OnCollisionEnter2D(Collision2D collision){
+        //GetComponent<Rigidbody2D>().velocity = (Vector2.up+Vector2.right)*startSpeed;
+
+    }
+
+    private void Update() {
+
+        if (GetComponent<Rigidbody2D>().velocity.y == 0f) {
+            Vector2 currentPosition = transform.position; //Posição inicial da bola
+            Vector2 positionPaddle = GameObject.Find("Paddle").transform.position; //Posição do paddle
+            currentPosition.x = positionPaddle.x;
+            GetComponent<Rigidbody2D>().MovePosition(currentPosition);
+            if (Input.GetKey(KeyCode.Space)) {
+                Debug.Log("start");
+                StartBall();
+            }
+        }
+
+              
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
 		Debug.Log (collision.collider.name);
 	}
 
@@ -22,4 +40,10 @@ public class BallBehaviour : MonoBehaviour {
 			Debug.Log ("GameOver");
 		}
 	}
+
+    void StartBall() {
+        GetComponent<Rigidbody2D>().velocity = (Vector2.up + Vector2.right) * startSpeed;
+    }
+
+    
 }
---
+++ Arkanoid/Assets/Scripts/BrickCrackBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCrackBehaviour : MonoBehaviour {
+
+    public Sprite[] spriteList;
+    public int maxHits;
+    protected int hitNumbers;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        hitNumbers = 0;
+    }
+
+    private void OnCollisionExit2D()
+    {
+
+        hitNumbers++;
+
+        if (hitNumbers >= maxHits)
+        {
+
+            Destroy(gameObject);
+        } else {
+            GetComponent<SpriteRenderer>().sprite = spriteList[hitNumbers];
+        }
+
+    }
+}
